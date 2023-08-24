544e6e2ee51c9874b69a7b9ea86c20007fd316e4
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-08-24 10:28:05 +0100

    Fixed loading animation😒️😮‍💨️

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -1,11 +1,10 @@
 "use client";
-
-import { HashLoader } from "react-spinners";
+import { MoonLoader } from "react-spinners";
 
 function Loading() {
   return (
-    <div className="flex justify-center items-center">
-      <HashLoader color="#0000FF" size={100} />
+    <div className="flex justify-center items-center h-full mt-[20%]">
+      <MoonLoader color="#0000FF" size={100} />
     </div>
   );
 }
