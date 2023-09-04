2f0793b3763da2483a7578545e5ca4a35d3837fb
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-09-04 13:40:09 +0100

    Added Sidebar with toggle fuctionality👌👌

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,27 +1,38 @@
+"use client";
 import { menuitems } from "./menuitems";
 import dynamic from "next/dynamic";
-import Link from "next/link";
 import { BsFillPersonFill } from "react-icons/bs";
+import SideBar from "./Sidebar";
+import { useState } from "react";
 
 const Menuitems = dynamic(() => import("../components/Menuitems"), {
   ssr: false,
 });
 
 function Navbar() {
+  const [sideOpen, setSideOpen] = useState(false);
   return (
-    <div>
+    <div
+      className={
+        sideOpen
+          ? "fixed top-0 left-0 w-full h-full bg-neutral-900/90 backdrop-blur z-[14]"
+          : ""
+      }
+    >
       <nav className="bg-neutral-900 fixed top-0 lg:py-4 py-3 w-full z-10 lg:flex block lg:justify-between border-b-[0.01px] border-neutral-700/80">
         <ul className="lg:flex block space-x-4 lg:mx-8">
           {menuitems.map((item, index) => (
             <Menuitems item={item} key={index} />
           ))}
         </ul>
-        <button className="w-[30px] h-[30px] rounded-[50%] hover:opacity-75 flex justify-center bg-neutral-500 items-center mr-2">
-          <Link href={"/account"}>
-            <BsFillPersonFill size={20} />
-          </Link>
+        <button
+          className="w-[30px] h-[30px] rounded-[50%] hover:opacity-75 flex justify-center bg-neutral-500 items-center mr-2"
+          onClick={() => setSideOpen((prevopen) => !prevopen)}
+        >
+          <BsFillPersonFill size={20} />
         </button>
       </nav>
+      <SideBar className={sideOpen ? "block" : "hidden"} />
     </div>
   );
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,15 @@
+interface SideBarProps {
+  className?: string;
+}
+
+function SideBar({ className }: SideBarProps) {
+  return (
+    <div
+      className={`${className} w-[300px] h-full fixed right-0 top-[66px] rounded-l-[20px] rounded-tl-[20px] rounded-bl-[20px] bg-neutral-800 p-4 z-[15]`}
+    >
+      <p>999 shit</p>
+    </div>
+  );
+}
+
+export default SideBar;
